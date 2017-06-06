e7024fe33722b914e3084e71b06d74786113c1ef
Denes Pal <denes.pal@lab.coop>
2017-06-06 22:38:42 +0200

    Initial
    Signed-off-by: Denes Pal <denes.pal@lab.coop>

---
+++ src/index.js
@@ -0,0 +1,213 @@
+//@flow
+import flattenDeep from 'lodash.flattendeep';
+import compact from 'lodash.compact';
+import omit from 'lodash.omit';
+import mergeWith from 'lodash.mergewith';
+import intersection from 'lodash.intersection';
+import pick from 'lodash.pick';
+import pluralize from 'pluralize';
+import {
+  encode as firebaseKeyEncode,
+  decode as firebaseKeyDecode
+} from 'firebase-key';
+import camelcase from 'camelcase';
+
+export default function firebaseDenormalizer(firebase) {
+
+  return function modelDenormalizer(modelName) {
+    let filterables = {};
+
+    return Object.freeze({
+      filterableProperty,
+      nonFilterableProperty,
+      get,
+      getById,
+      push,
+      update,
+      updateById,
+      set,
+      setById,
+      remove,
+      removeById,
+      find
+    });
+
+    function filterableProperty(propName) {
+      filterables[propName] = true;
+    }
+
+    async function nonFilterableProperty(propName) {
+      filterables = omit(filterables, propName);
+      return firebase.ref(getDenormalizedCollectionName(modelName, propName))
+        .remove();
+    }
+
+    async function push(payload) {
+      const pushedId = firebase.ref(getPathString(modelName))
+        .push(payload).key;
+      await actionOnFilterables(modelName, payload, (joinCollectionName, filterableKey, filterableValue) => {
+        return firebase.ref(getPathString(joinCollectionName, getKey(filterableValue), pushedId))
+          .set(true);
+      });
+      return pushedId;
+    }
+
+    function pushTo(path, payload) {
+      return firebase.ref(getPathString(path))
+        .push(payload).key;
+    }
+
+    async function setById(id, payload) {
+      return set([modelName, id], payload);
+    }
+
+    async function set(path, payload) {
+      await remove(path);
+      const pathRef = firebase.ref(getPathString(path));
+      await pathRef.set(payload);
+      await actionOnFilterables(modelName, payload, (joinCollectionName, filterableKey, filterableValue) => {
+        return firebase.ref(getPathString(joinCollectionName, getKey(filterableValue), pathRef.key))
+          .set(true);
+      });
+      return payload;
+    }
+
+    async function updateById(id, payload) {
+      return update([modelName, id], payload);
+    }
+
+    async function update(path, payload) {
+      const pathObject = await get(path);
+      const pathRef = firebase.ref(getPathString(path));
+      await pathRef.update(payload);
+      await actionOnFilterables(modelName, payload, (joinCollectionName, filterableKey, filterableValue) => {
+        if (pathObject.hasOwnProperty(filterableKey) &&
+          pathObject[filterableKey] != filterableValue) {
+          return Promise.all([
+            firebase.ref(getPathString(joinCollectionName, pathObject[filterableKey], pathRef.key))
+              .remove(),
+            firebase.ref(getPathString(joinCollectionName, getKey(filterableValue), pathRef.key))
+              .set(true)
+          ]);
+        }
+      });
+      return payload;
+    }
+
+    async function removeById(id) {
+      return remove([modelName, id]);
+    }
+
+    async function remove(path) {
+      const pathObject = await get(path);
+      const pathRef = firebase.ref(getPathString(path));
+      await pathRef.remove();
+      return actionOnFilterables(modelName, pathObject, (joinCollectionName, filterableKey, filterableValue) => {
+        return firebase.ref(getPathString(joinCollectionName, getKey(filterableValue), pathRef.key))
+          .remove();
+      });
+    }
+
+    async function getById(id) {
+      return get([modelName, id]);
+    }
+
+    async function get(path) {
+      const snapshot = await firebase.ref(getPathString(path))
+        .once('value');
+      return snapshot.val();
+    }
+
+    async function find(payload) {
+      const findActions = [];
+      for (let filterKey in payload) {
+        const filterValue = payload[filterKey];
+        if (filterables.hasOwnProperty(filterKey)) {
+          findActions.push(filterByDenormalizedProperty(modelName, filterKey, filterValue));
+        } else {
+          findActions.push(filterByNonDenormalizedProperty(modelName, filterKey, filterValue));
+        }
+      }
+      const resultsIntersect = getFiltersIntersection(await Promise.all(findActions));
+      const completeResults = await completeResultObjects(resultsIntersect);
+      return completeResults;
+    }
+
+    async function filterByDenormalizedProperty(modelName, filterKey, filterValue) {
+      const denormalizedCollectionName = getDenormalizedCollectionName(modelName, filterKey);
+      const path = getPathString(denormalizedCollectionName, getKey(filterValue));
+      const snapshot = await firebase.ref(path).once('value');
+      return snapshot.val();
+    }
+
+    async function filterByNonDenormalizedProperty(modelName, filterKey, filterValue) {
+      const path = getPathString(modelName);
+      const snapshot = await firebase.ref(path).orderByChild(filterKey).equalTo(filterValue).once('value');
+      return snapshot.val();
+    }
+
+    function getFiltersIntersection(filterResults: Array<object>) {
+      const mergedFilterResults = filterResults.reduce((acc, val) => mergeWith(acc, val, fullObjectPriority), {});
+      const ids = filterResults.map(filterResult => Object.keys(filterResult));
+      const intersectionIds = intersection.apply(null, ids);
+      return pick(mergedFilterResults, intersectionIds);
+    }
+
+    function completeResultObjects(results: {}) {
+      return Promise.all(Object.keys(results).map(resultKey => completeResultObject(results, resultKey)));
+    }
+
+    function completeResultObject(results: {}, resultKey: string) {
+      const resultObject = results[resultKey];
+      if (isCompleteResultObject(resultObject)) {
+        return resultObject;
+      } else {
+        return getById(resultKey);
+      }
+    }
+
+    function isCompleteResultObject(resultObject) {
+      return typeof resultObject === 'object';
+    }
+
+    async function actionOnFilterables(modelName, payload, action) {
+      const actions = [];
+      for (let filterableKey in payload) {
+        if (filterables.hasOwnProperty(filterableKey)) {
+          const filterableValue = payload[filterableKey];
+          const joinCollectionName = getDenormalizedCollectionName(modelName, filterableKey);
+          actions.push(action(joinCollectionName, filterableKey, filterableValue));
+        }
+      }
+      return Promise.all(actions);
+    }
+  }
+}
+
+export function fullObjectPriority(objValue, srcValue) {
+  if (typeof objValue === 'object' || typeof srcValue === 'object') {
+    return (typeof objValue === 'object') ?
+      Object.assign({}, objValue, srcValue) :
+      Object.assign({}, srcValue, objValue);
+  } else {
+    return srcValue;
+  }
+}
+
+export function getKey(name) {
+  return firebaseKeyEncode(camelcase(name));
+}
+
+export function getDenormalizedCollectionName(modelName, joinModel) {
+  return getKey(`${pluralize.singular(modelName)} ${pluralize.plural(joinModel)}`);
+}
+
+export function getPathArray(...paths) {
+  return getPathString(...paths).split('/');
+}
+
+export function getPathString(...paths) {
+  paths = compact(flattenDeep(paths));
+  return `${compact(flattenDeep(paths.map(path => path.split('/')))).join('/')}`
+    .replace(/\/$/, '');
+}
---
+++ src/index.test.js
@@ -0,0 +1,221 @@
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
+import {
+  default as FirebaseDenormalizer,
+  getPathString,
+  getPathArray,
+  getDenormalizedCollectionName,
+  getKey
+} from './index';
+
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyDLOW4voY8C_v-ClaqbtnwnT0O3rmqv1-c",
+  authDomain: "fir-denormalizer.firebaseapp.com",
+  databaseURL: "https://fir-denormalizer.firebaseio.com",
+  projectId: "fir-denormalizer",
+  storageBucket: "fir-denormalizer.appspot.com",
+  messagingSenderId: "949167701661"
+};
+firebase.initializeApp(config);
+
+/*
+* firebase.ref()
+* */
+
+const modelDenormalizer = FirebaseDenormalizer(firebase.database());
+const dogs = modelDenormalizer('dogs');
+
+describe.skip('getPathString', () => {
+  it('should work with one string parameter', () => {
+    expect(getPathString()).toBe('');
+    expect(getPathString('/')).toBe('');
+    expect(getPathString('dogs')).toBe('dogs');
+    expect(getPathString('/dogs')).toBe('dogs');
+    expect(getPathString('/dogs/kennels')).toBe('dogs/kennels');
+    expect(getPathString('dogs/kennels')).toBe('dogs/kennels');
+  });
+
+  it('should work with multiple string parameter', () => {
+    expect(getPathString('dogs', 'kennels')).toBe('dogs/kennels');
+    expect(getPathString('dogs', '/kennels')).toBe('dogs/kennels');
+    expect(getPathString('dogs', '/kennels/')).toBe('dogs/kennels');
+    expect(getPathString('dogs', '/kennels/')).toBe('dogs/kennels');
+    expect(getPathString('dogs', '/kennels/', '', 'wolfdog')).toBe('dogs/kennels/wolfdog');
+  });
+
+  it('should work with multiple array', () => {
+    expect(getPathString([''], ['kennels'])).toBe('kennels');
+    expect(getPathString(['dogs'], ['kennels'])).toBe('dogs/kennels');
+    expect(getPathString(['dogs'], [''])).toBe('dogs');
+    expect(getPathString(['dogs'], ['kennels', 'wolfdog'], ['othertype'])).toBe('dogs/kennels/wolfdog/othertype');
+  });
+
+  it('should work with combined array and contated string parameters', () => {
+    expect(getPathString('dogs/kennels', ['wolfdog'])).toBe('dogs/kennels/wolfdog');
+  });
+
+});
+
+describe.skip('getKey', () => {
+  it('should encode keys', () => {
+    expect(getKey('')).toBe('');
+    expect(getKey('key')).toBe('key');
+    expect(getKey('John Doe')).toBe('johnDoe');
+    expect(getKey('John--Doe')).toBe('johnDoe');
+    expect(getKey('John/Doe/')).toBe('john!2FDoe!2F');
+    expect(getKey('John.Doe.')).toBe('johnDoe');
+    expect(getKey('John\'s kennel')).toBe('john\'sKennel');
+  });
+});
+
+describe.skip('getDenormalizedCollectionName', () => {
+  it('should encode keys', () => {
+    expect(getDenormalizedCollectionName('', '')).toBe('');
+    expect(getDenormalizedCollectionName('john', 'doe')).toBe('johnDoes');
+    expect(getDenormalizedCollectionName('john doe', 'john doe')).toBe('johnDoeJohnDoes');
+  });
+});
+
+describe('denormalizer', () => {
+
+  beforeEach(() => {
+    firebase.database().ref().remove();
+  });
+
+  it.skip('should push, and remove', async () => {
+    dogs.filterableProperty('type');
+    dogs.filterableProperty('kennel');
+
+    const aDog = {
+      type: 'wolfdog',
+      kennel: "John's kennel"
+    };
+
+    const id1 = await dogs.push(aDog);
+    const id2 = await dogs.push(aDog);
+    expect(id1).toBeTruthy();
+    expect(id2).toBeTruthy();
+
+    expect(await dogs.getById(id1)).toEqual(aDog);
+    expect(await dogs.get(['dogTypes', 'wolfdog', id1])).toBe(true);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id1])).toBe(true);
+
+    expect(await dogs.getById(id2)).toEqual(aDog);
+    expect(await dogs.get(['dogTypes', 'wolfdog', id2])).toBe(true);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id2])).toBe(true);
+
+    await dogs.removeById(id1);
+    expect(await dogs.getById(id1)).toBe(null);
+    expect(await dogs.get(['dogTypes', 'wolfdog', id1])).toBe(null);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id1])).toBe(null);
+    expect(await dogs.getById(id2)).toEqual(aDog);
+    expect(await dogs.get(['dogTypes', 'wolfdog', id2])).toBe(true);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id2])).toBe(true);
+
+    await dogs.removeById(id2);
+    expect(await dogs.getById(id1)).toBe(null);
+    expect(await dogs.get(['dogTypes', 'wolfdog', id1])).toBe(null);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id1])).toBe(null);
+    expect(await dogs.getById(id2)).toEqual(null);
+    expect(await dogs.get(['dogTypes', 'wolfdog', id2])).toBe(null);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id2])).toBe(null);
+  });
+
+  it.skip('should push and update', async () => {
+    dogs.filterableProperty('type');
+    dogs.filterableProperty('kennel');
+
+    const aDog = {
+      type: 'wolfdog',
+      kennel: "John's kennel"
+    };
+
+    const id1 = await dogs.push(aDog);
+    const id2 = await dogs.push(aDog);
+
+    await dogs.updateById(id1, { name: 'newName' });
+    expect(await dogs.getById(id1)).toEqual({...aDog, name: 'newName'});
+    expect(await dogs.getById(id2)).toEqual(aDog);
+
+    await dogs.updateById(id1, { type: 'pumi' });
+    expect(await dogs.getById(id1)).toEqual({...aDog, name: 'newName', type: 'pumi'});
+    expect(await dogs.get(['dogTypes', 'wolfdog', id1])).toBe(null);
+    expect(await dogs.get(['dogTypes', 'pumi', id1])).toBe(true);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id1])).toBe(true);
+
+    await dogs.updateById(id1, { type: 'wolfdog' });
+    expect(await dogs.getById(id1)).toEqual({...aDog, name: 'newName', type: 'wolfdog'});
+    expect(await dogs.get(['dogTypes', 'wolfdog', id1])).toBe(true);
+    expect(await dogs.get(['dogTypes', 'pumi', id1])).toBe(null);
+    expect(await dogs.get(['dogKennels', 'john\'sKennel', id1])).toBe(true);
+
+    expect(await dogs.getById(id2)).toEqual(aDog);
+  });
+
+  it.skip('should push and set', async () => {
+    dogs.filterableProperty('type');
+    dogs.filterableProperty('kennel');
+
+    const aDog = {
+      type: 'wolfdog',
+      kennel: "John's kennel"
+    };
+
+    const id1 = await dogs.push(aDog);
+    const id2 = await dogs.push(aDog);
+
+    await dogs.setById(id1, { name: 'new name' });
+    expect(await dogs.getById(id1)).toEqual({ name: 'new name' });
+    expect(await dogs.getById(id2)).toEqual(aDog);
+
+    await dogs.setById(id1, { name: 'new name', type: 'pumi', kennel: 'Barbara\'s kennel' });
+    expect(await dogs.getById(id1)).toEqual({name: 'new name', type: 'pumi', kennel: 'Barbara\'s kennel' });
+    expect(await dogs.get(['dogTypes', 'wolfdog', id1])).toBe(null);
+    expect(await dogs.get(['dogTypes', 'pumi', id1])).toBe(true);
+
+    expect(await dogs.getById(id2)).toEqual(aDog);
+  });
+
+  it('should find by properties', async () => {
+    dogs.filterableProperty('type');
+    dogs.filterableProperty('kennel');
+
+    await dogs.push({ name: 'Petee', type: 'wolfdog', kennel: "John's kennel"});
+    await dogs.push({ name: 'Lolly', type: 'wolfdog', kennel: "John's kennel"});
+    await dogs.push({ type: 'pumi', kennel: "John's kennel"});
+    await dogs.push({ type: 'pumi', kennel: "Barbara's kennel"});
+    await dogs.push({ type: 'puli', kennel: "Barbara's kennel"});
+
+    expect(await dogs.find({
+      name: 'Petee'
+    })).toHaveLength(1);
+
+    expect(await dogs.find({
+      name: 'Petee',
+      kennel: 'John\'s kennel'
+    })).toHaveLength(1);
+
+    expect(await dogs.find({
+      kennel: 'John\'s kennel'
+    })).toHaveLength(3);
+
+    expect(await dogs.find({
+      type: 'wolfdog',
+      kennel: 'John\'s kennel'
+    })).toHaveLength(2);
+
+    expect(await dogs.find({
+      type: 'pumi',
+      kennel: 'John\'s kennel'
+    })).toHaveLength(1);
+
+    expect(await dogs.find({
+      type: 'puli',
+      kennel: 'John\'s kennel'
+    })).toHaveLength(0);
+
+  });
+
+});
